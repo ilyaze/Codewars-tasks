8c065da308804dba2b827dd4404e962d4c9accc1
Ilya Zaitsev <82403860+ilyaze@users.noreply.github.com>
2022-07-03 00:15:08 +0300

    Update Length and two values (python).py

--- 7 kyu/Length and two values (python).py
+++ 7 kyu/Length and two values (python).py
@@ -1,6 +1,5 @@
 def alternate(n, first_value, second_value):
     a, b = [], 0
     while b != n:
-        a.append(first_value); a.append(second_value)
-        b += 1
+        a.append(first_value); a.append(second_value); b += 1
     return a[0:n]
