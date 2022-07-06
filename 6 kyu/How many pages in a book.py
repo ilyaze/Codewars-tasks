cdada8b1098b31b43681cd73d23e1c9d0dee8c58
Ilya Zaitsev <82403860+ilyaze@users.noreply.github.com>
2022-07-06 23:38:39 +0300

    Delete 6 kyu directory

--- 6 kyu/How many pages in a book.py
+++
@@ -1,8 +0,0 @@
-def amount_of_pages(summary):
-    pages = 0
-    page_num = 1
-    while summary > 0:
-        summary -= len(str(page_num))
-        pages += 1
-        page_num += 1
-    return pages
