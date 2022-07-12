7fb1f7636862a4c8ca16cb2bbd6bdc0d24411a8f
Ilya Zaitsev <82403860+ilyaze@users.noreply.github.com>
2022-07-12 23:07:16 +0300

    Add files via upload

---
+++ Python/6 kyu/Find the unique number.py
@@ -0,0 +1,14 @@
+def find_uniq(arr):
+    a = []
+    
+    if arr[0] == arr[1]:
+        a = arr[0]
+    elif arr[1] == arr[2]:
+        a = arr[1]
+    elif arr[2] == arr[3]:
+        a = arr[2]
+    for i in arr:
+        if i == a:
+            continue
+        else:
+            return i
